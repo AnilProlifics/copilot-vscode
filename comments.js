3ea0cd861c96abe3393b87e9dd6a47d880fafb05
Anil Kumar <98883147+AnilProlifics@users.noreply.github.com>
2023-07-21 23:46:24 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,16 @@
 // create web server
-console.log("server created");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // Check the request path
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Hello, World!</h1>");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
+});
+
+server.listen(8080);
+console.log("Server is running on port 8080");
